2e470402fe9bdb82f1b7665674876cbce514ea8a
Luis Maldonado <luis.maldonado.1366@gmail.com>
2024-05-22 23:40:40 -0600

    Constructors and getters for models

--- src/models/Card.java
+++ src/models/Card.java
@@ -1,5 +1,43 @@
 package models;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Card {
 
+    private double creditLimit;
+    private double balance;
+    private List<Item> itemList;
+
+    public Card(double creditLimit) {
+
+        this.creditLimit = creditLimit;
+        this.balance = creditLimit;
+        this.itemList = new ArrayList<>();
+
+    }
+
+    public boolean buyItem(Item item) {
+        boolean result = false;
+
+        if (this.balance >= item.getValue()) {
+            this.balance -= item.getValue();
+            this.itemList.add(item);
+            result = true;
+        }
+        return result;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<Item> getItemList() {
+        return itemList;
+    }
 }
--- src/models/Item.java
+++ src/models/Item.java
@@ -1,4 +1,27 @@
 package models;
 
 public class Item {
+    private double value;
+    private String title;
+
+    public Item(double value, String title) {
+        this.value = value;
+        this.title = title;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public String getTitle() {
+        return title;0
+    }
+
+    @Override
+    public String toString() {
+        return "Item: " +
+                "value: " + value +
+                ", title:'" + title + '\'';
+
+    }
 }
